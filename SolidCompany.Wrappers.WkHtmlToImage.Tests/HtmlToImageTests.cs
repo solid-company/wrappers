9905ab50028393d5c805ae2a267fe3a60c48a9ba
Wojciech Torkowski <7881551+wojciech-torkowski@users.noreply.github.com>
2021-08-01 17:38:05 +0200

    Added tests

--- SolidCompany.Wrappers.WkHtmlToImage.Tests/HtmlToImageTests.cs
+++ SolidCompany.Wrappers.WkHtmlToImage.Tests/HtmlToImageTests.cs
@@ -1,4 +1,6 @@
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging.Abstractions;
 using NUnit.Framework;
 using SolidCompany.Wrappers.WkHtmlToImage.Registration;
@@ -16,6 +18,20 @@ namespace SolidCompany.Wrappers.WkHtmlToImage.Tests
             };
 
             using var htmlToImage = new HtmlToImage(options, NullLoggerFactory.Instance);
+
+            var stream = await htmlToImage.CreateImageAsync("<html><body style=\"background-color: red\"></body></html>", 100, ImageFormat.Png);
+
+            Assert.That(stream.Length, Is.GreaterThan(0));
+        }
+
+        [Test]
+        public async Task Can_generate_image_from_host()
+        {
+            var host = new HostBuilder()
+                .ConfigureServices(services => services.AddHtmlToImageConversion((sp, options) => { }))
+                .Build();
+
+            var htmlToImage = host.Services.GetRequiredService<IHtmlToImage>();
 
             var stream = await htmlToImage.CreateImageAsync("<html><body style=\"background-color: red\"></body></html>", 100, ImageFormat.Png);
 
